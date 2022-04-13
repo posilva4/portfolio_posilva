87fd7d6632b376d7ad9806e07f548351cfe2404e
Pedro <posilva4@gmail.com>
2022-04-13 15:08:14 +0100

    fix: test commit ssh

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <>
     <Header/>
-    <Nav/>
+    <Nav/>    
     <About/>
     <Experience/>
     <Services/>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/posilvaHome1.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Pedro Silva</h1>
+        <h5 className="text-light">.Net Developer</h5>
+        <CTA />
+        <div className='me'>         
+          <img src={ME} alt="me" />         
+        </div>
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+        <HeaderSocials />
+      </div>      
+    </header>
   )
 }
 
